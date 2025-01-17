8221e2d999ea2e923d61087545df504a9f7615b8
Hadeel Abdellatif <hadeel.abdellatif2001@gmail.com>
2025-01-17 16:18:43 +0200

    Responsive Website

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#272343',
+        gray: '#9A9CAA',
+        secondary: '#007580',
+        gray2: '#636270',
+        gray3: '#E1E3E5',
+      },
+      fontFamily: {
+        Inter: ['Inter', 'sans-serif'],
+      },
+      screens: {
+        '2md': "960px",
+        phones: '390px',
+      },
+      spacing: {
+        '300': '300px',
+        '0.2': '0.05rem',
+      },
+    },
   },
   plugins: [],
 }
